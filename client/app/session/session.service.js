8b215d37a9cfe94aead93bc3e3fecd8aad592800
UdayaR <udaya20690@gmail.com>
2016-01-23 23:09:13 +0530

    client: session: Add factory 'session'
    Used to hold the details of the session
    
    Signed-off-by: UdayaR <udaya20690@gmail.com>

---
+++ client/app/session/session.service.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('pokerVirtualMoneyApp')
+  .factory('session', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
---
+++ client/app/session/session.service.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Service: session', function () {
+
+  // load the service's module
+  beforeEach(module('pokerVirtualMoneyApp'));
+
+  // instantiate service
+  var session;
+  beforeEach(inject(function (_session_) {
+    session = _session_;
+  }));
+
+  it('should do something', function () {
+  });
+
+});
