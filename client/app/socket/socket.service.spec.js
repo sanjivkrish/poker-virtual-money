4faeffd8cc5921145a3f8469f7816ec0e3596eec
udaya <udaya20690@gmail.com>
2016-01-23 15:31:31 +0530

    client: socket: Add factory 'socket'
    command:
    $ yo angular-fullstack:factory socket
    ? Where would you like to create this factory? (client/app/)
    ? Where would you like to create this factory? client/app/
       create client\app\socket\socket.service.js
       create client\app\socket\socket.service.spec.js
    
    Signed-off-by: udaya <udaya20690@gmail.com>

---
+++ client/app/socket/socket.service.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Service: socket', function () {
+
+  // load the service's module
+  beforeEach(module('pokerVirtualMoneyApp'));
+
+  // instantiate service
+  var socket;
+  beforeEach(inject(function (_socket_) {
+    socket = _socket_;
+  }));
+
+  it('should do something', function () {
+  });
+
+});
