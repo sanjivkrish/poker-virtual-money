d8089920b19ddcac2c4466cb42249646a59f09ce
UdayaR <udaya20690@gmail.com>
2016-01-23 22:40:51 +0530

    client: login: Add promise for login function
    If valid, change to home state, else be in a same state
    
    Signed-off-by: UdayaR <udaya20690@gmail.com>

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -1,10 +1,20 @@
 'use strict';
 
 angular.module('pokerVirtualMoneyApp')
-  .controller('LoginCtrl', function ($scope, socket) {
+  .controller('LoginCtrl', function ($scope, $state, socket) {
 
     $scope.login = function() {
         // your code goes here
-         socket.emit({type:'login', data:{name: $scope.user.name, password: $scope.user.pwd}});
+        var promise = socket.emit({type:'login', data:{name: $scope.user.name, password: $scope.user.pwd}}, promise);
+        promise.then(function(obj) {
+            if (obj.status) {
+                $state.go('home');
+            } else {
+                // wrong password
+            }
+        },
+        function() {
+            console.log("socket communication failed");
+        });
     };
   });
