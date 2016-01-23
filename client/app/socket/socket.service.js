f70423e61100f62144c430578497d071431ba744
UdayaR <udaya20690@gmail.com>
2016-01-23 22:39:59 +0530

    client: socket: Add promise for socket events
    Signed-off-by: UdayaR <udaya20690@gmail.com>

--- client/app/socket/socket.service.js
+++ client/app/socket/socket.service.js
@@ -1,9 +1,11 @@
 'use strict';
 
 angular.module('pokerVirtualMoneyApp')
-  .factory('socket', function () {
+  .factory('socket', function ($q) {
 
     var socket = {};
+
+    var deferred;
 
     socket.ws = new WebSocket('ws://localhost:9000');
 
@@ -12,15 +14,23 @@ angular.module('pokerVirtualMoneyApp')
     //
     socket.ws.onmessage = function(message) {
         var obj = JSON.parse(message.data);
-        // Logic goes here
-         console.log(obj);
+        //
+        // All broadcast message from server has a type 'broadcast'
+        //
+        if (obj.type !== 'broadcast') {
+            deferred.resolve(obj);
+        } else {
+            // Logic goes here
+        }
     };
 
     //
     // Message to serve
     //
     socket.emit = function(message) {
+        deferred = $q.defer();
         socket.ws.send(JSON.stringify(message));
+        return deferred.promise;
     };
 
     return socket;
