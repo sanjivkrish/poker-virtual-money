8e30d130d0bacf9499f10df02228a30008ea76bc
sanjivkrish <sanjiv436.k@gmail.com>
2016-01-23 17:21:23 +0530

    server: login: Add operations on user details
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- server/lib/data-interface/login.js
+++ server/lib/data-interface/login.js
@@ -1,3 +1,41 @@
-/*
-Data interface file to compute user information
-*/
+//
+// List of all user
+//
+var users = [];
+
+//
+// Check if User exist
+//
+var returnUserPosition = function(name) {
+    for (var i in users) {
+        if (users[i].name === name) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+//
+// Return user information
+//
+exports.getUser = function(name){
+    var index = returnUserPosition(name);
+    if ( index !== -1) {
+        return users[index];
+    } else {
+        return null;
+    }
+};
+
+//
+// Set user infromation
+//
+exports.setUser = function(obj) {
+    if (returnUserPosition(obj.name) === -1) {
+        users.push(obj);
+    } else {
+        return 'User already Exist';
+    }
+        
+};
+
