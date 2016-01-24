51efe12386ccd58f6945387e3a8bb857e679fa85
UdayaR <udaya20690@gmail.com>
2016-01-24 12:43:46 +0530

    client: login: set current user details in session
    Remove unused promise argument
    
    Signed-off-by: UdayaR <udaya20690@gmail.com>

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('pokerVirtualMoneyApp')
-  .controller('LoginCtrl', function ($scope, $state, socket) {
+  .controller('LoginCtrl', function ($scope, $state, socket, session) {
 
     $scope.login = function() {
         // your code goes here
-        var promise = socket.emit({type:'login', data:{name: $scope.user.name, password: $scope.user.pwd}}, promise);
+        var promise = socket.emit({type:'login', data:{name: $scope.user.name, password: $scope.user.pwd}});
         promise.then(function(obj) {
             if (obj.status) {
+                session.setCurrentUser(obj.info)
                 $state.go('home');
             } else {
                 // wrong password
