6723c16a808a2cd9d3b1d2abe57e2ea6b4314421
UdayaR <udaya20690@gmail.com>
2016-01-24 13:22:18 +0530

    Server: lobbydetails: Add getLobbyInfo
    Signed-off-by: UdayaR <udaya20690@gmail.com>

--- server/lib/core/lobbydetails.js
+++ server/lib/core/lobbydetails.js
@@ -1,3 +1,10 @@
 /*
     Hold API's related to lobby details
 */
+var config = require('../config.json');
+var loginInfo = require('../data-interface/login.js');
+
+exports.getLobbyInfo = function() {
+    config.users = loginInfo.getAllUsers();
+    return config;
+};
