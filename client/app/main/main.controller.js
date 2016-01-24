a41cf885ede42057995df566a52f714cf8640320
UdayaR <udaya20690@gmail.com>
2016-01-24 13:24:19 +0530

    Client: main: Display all users in home page
    Signed-off-by: UdayaR <udaya20690@gmail.com>

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('pokerVirtualMoneyApp')
-  .controller('MainCtrl', function () {
-
+  .controller('MainController', function ($scope, $state, session, socket) {
+        var user = session.getCurrentUser();
+        if (user === undefined) {
+            $state.go('login');
+        } else {
+            var promise = socket.emit({type:'lobbydetails', data:{name: session.user.name}});
+            promise.then(function(obj) {
+                if (obj) {
+                    $scope.users = obj.users;
+                } else {
+                    // wrong password
+                }
+            },
+            function() {
+                console.log("socket communication failed");
+            });
+        }
   });
