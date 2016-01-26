f745abf8cf046c986e916271238d327e6522df65
sanjivkrish <sanjiv436.k@gmail.com>
2016-01-26 22:10:31 +0530

    client: arenInfo: Add service to hold arena info
    command:
    $ yo angular-fullstack:factory arenaInfo
    ? Where would you like to create this factory? (client/app/)
    ? Where would you like to create this factory? client/app/
       create client\app\arenaInfo\arenaInfo.service.js
       create client\app\arenaInfo\arenaInfo.service.spec.js
    
    
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

---
+++ client/app/arenaInfo/arenaInfo.service.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Service: arenaInfo', function () {
+
+  // load the service's module
+  beforeEach(module('pokerVirtualMoneyApp'));
+
+  // instantiate service
+  var arenaInfo;
+  beforeEach(inject(function (_arenaInfo_) {
+    arenaInfo = _arenaInfo_;
+  }));
+
+  it('should do something', function () {
+  });
+
+});
