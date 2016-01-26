d19eba3cf4357859fc2cdb9292ac95b71198edd1
sanjivkrish <sanjiv436.k@gmail.com>
2016-01-26 20:33:45 +0530

    client: arena: Display new user in the UI
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/arena/arena.controller.js
+++ client/app/arena/arena.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('pokerVirtualMoneyApp')
-  .controller('ArenaCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ArenaCtrl', function ($scope, socket, $rootScope) {
+    socket.sendRequest('getUsers').then(function(obj) {
+        $scope.players = obj;
+    }, function(err) {
+        console.log(err);
+    });
+    
+    $rootScope.$on('updateusers', function(evt, obj) {
+        $scope.players = obj;
+        $scope.$digest();
+    });
   });
