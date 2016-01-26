6a1532a88f9519dcad9c702e0ceb1270ac6b13d7
sanjivkrish <sanjiv436.k@gmail.com>
2016-01-26 13:05:27 +0530

    client: login: Replace login emit with request
    Request expects response.
    Emit sends the data to server
    
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -4,18 +4,24 @@ angular.module('pokerVirtualMoneyApp')
   .controller('LoginCtrl', function ($scope, $state, socket, session) {
 
     $scope.login = function() {
-        // your code goes here
-        var promise = socket.emit({type:'login', data:{name: $scope.user.name, password: $scope.user.pwd}});
-        promise.then(function(obj) {
+        //
+        // User info
+        //
+        var data = {
+            name: $scope.user.name,
+            password: $scope.user.pwd
+        };
+
+        socket.sendRequest('login', data).then(function(obj) {
             if (obj.status) {
                 session.setCurrentUser(obj.info)
                 $state.go('home');
             } else {
                 // wrong password
             }
-        },
-        function() {
-            console.log("socket communication failed");
+        }, function(err) {
+            console.log(err);
         });
     };
+
   });
