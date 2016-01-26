f286d0d36c2c094439b92fe1f90bf49cd02de731
sanjivkrish <sanjiv436.k@gmail.com>
2016-01-26 16:55:38 +0530

    client: login: validate user before moving to login state
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -4,8 +4,17 @@ angular.module('pokerVirtualMoneyApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('login', {
-        url: '/login',
+        url: '/',
         templateUrl: 'app/login/login.html',
-        controller: 'LoginCtrl'
+        controller: 'LoginCtrl',
+        resolve: {
+            loginSession : ['session', '$state', function (session, $state) {
+                session.isLoginValid().then(function() {
+                    return '';
+                }, function() {
+                    $state.go('main');
+                });
+            }]
+        }
       });
   });
