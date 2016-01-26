9f4160c6352c4f89d45ff0fbf9c75256e2bf7b38
sanjivkrish <sanjiv436.k@gmail.com>
2016-01-26 16:54:46 +0530

    client: main: Remove user validation
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,12 +1,6 @@
 'use strict';
 
 angular.module('pokerVirtualMoneyApp')
-  .controller('MainController', function ($scope, $state, session) {
-        $scope.user = session.getCurrentUser();
-
-        if (Object.keys($scope.user).length === 0) {
-            $state.go('login');
-        } else {
-            // Rediect to arena controller
-        }
+  .controller('MainController', function () {
+    
   });
