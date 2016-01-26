e4b74a79e9c388334a6d46e1bebbc1936fe587fd
sanjivkrish <sanjiv436.k@gmail.com>
2016-01-26 13:04:29 +0530

    client: login: Remove unwanted comments from login.js
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -1,14 +1,3 @@
-//'use strict';
-//
-//angular.module('pokerVirtualMoneyApp')
-//  .config(function ($routeProvider) {
-//    $routeProvider
-//      .when('/login', {
-//        templateUrl: 'app/login/login.html',
-//        controller: 'LoginCtrl'
-//      });
-//  });
-
 'use strict';
 
 angular.module('pokerVirtualMoneyApp')
